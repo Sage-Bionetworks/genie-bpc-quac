268240f29aef65b3c0b0f4798a64de985a7cbc12
Thomas Yu <thomas.yu@sagebase.org>
2024-01-27 13:46:07 -0800

    Add python functinos

---
+++ quac.py
@@ -0,0 +1,347 @@
+#!/usr/bin/env python
+
+# Description: Command line interface for conducting quality assurance (QA) checks
+#   on BPC cohorts and sites for different stages of processing.
+# Author: Haley Hunter-Zinck
+# Date: June 10, 2021
+
+# pre-setup ---------------------------------
+
+import argparse
+import yaml
+import os
+
+# start timer
+import time
+
+tic = time.time()
+
+# libraries
+import pandas as pd
+
+# import openpyxl
+import json
+import synapseclient
+
+# from glue import glue
+
+# functions
+# replace with your actual function files
+from utils import *
+from checklist import *
+
+workdir = "."
+if not os.path.exists("config.yaml"):
+    workdir = "/usr/local/src/myscripts"
+with open(os.path.join(workdir, "config.yaml"), "r") as stream:
+    config = yaml.safe_load(stream)
+
+choices_report = sorted(config["report"].keys())
+choices_number = [
+    int(k)
+    for k, v in config["checks"].items()
+    if v["implemented"] and not v["deprecated"]
+]
+choices_level = sorted(
+    set([v["level"] for v in config["checks"].values() if v["level"] != "fail"])
+)
+choices_cohort = sorted(config["uploads"].keys())
+choices_site = sorted(
+    set([item for sublist in config["uploads"].values() for item in sublist])
+)
+choice_all = "all"
+
+# cli ------------------------------------------------
+
+# parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-c",
+    dest="cohort",
+    type=str,
+    choices=choices_cohort,
+    help="Cohort on which to perform checks",
+    required=True,
+)
+parser.add_argument(
+    "-s",
+    dest="site",
+    type=str,
+    default=choice_all,
+    choices=[choice_all] + choices_site,
+    help="Site on which to perform checks",
+    required=False,
+)
+parser.add_argument(
+    "-r",
+    dest="report",
+    type=str,
+    choices=choices_report,
+    help="Report to generate",
+    required=True,
+)
+parser.add_argument(
+    "-n",
+    dest="number",
+    type=int,
+    help="Reference number of check to run individually within report",
+    required=False,
+)
+parser.add_argument(
+    "-l",
+    dest="level",
+    type=str,
+    default=choice_all,
+    choices=[choice_all] + choices_level,
+    help="Level of priority of checks to run",
+    required=False,
+)
+parser.add_argument(
+    "-o",
+    dest="overview",
+    action="store_true",
+    default=False,
+    help="Display overview of parameters and checks to be performed but do not execute",
+)
+parser.add_argument(
+    "-u",
+    dest="save_to_synapse",
+    action="store_true",
+    default=False,
+    help="Save report log file to pre-specified Synapse folder",
+)
+parser.add_argument(
+    "-v",
+    dest="verbose",
+    action="store_true",
+    default=False,
+    help="Display messages on script progress to the user",
+)
+parser.add_argument(
+    "-a",
+    dest="synapse_auth",
+    type=str,
+    default="~/.synapseConfig",
+    help="Path to .synapseConfig file or Synapse PAT (default: '~/.synapseConfig')",
+)
+
+# extract command line arguments
+args = parser.parse_args()
+report = args.report
+number = args.number
+level = args.level
+cohort = args.cohort
+sites = args.site
+verbose = args.verbose
+save_synapse = args.save_to_synapse
+overview = args.overview
+synapse_auth = args.synapse_auth
+
+# setup ------------------------------------------------------------------------
+
+
+# synapse login
+syn = synapseclient.login()
+
+# update config
+config = update_config_for_comparison_report(config)
+config = update_config_for_release_report(config)
+
+# check user input -------------------------------------------------------------
+
+if number is not None:
+    # number should be relevant for report
+    check_nos = config["report"][report]
+    if number not in check_nos:
+        msg1 = f"check number '{number}' is not applicable for report type '{report}'. "
+        msg2 = f"To view applicable check numbers, run the following: "
+        msg3 = f"'python genie-bpc-quac.py -c {cohort} -r upload -o'"
+        raise ValueError(msg1 + msg2 + msg3)
+
+    # number should be relevant to number
+    check_no_level = config["checks"][number]["level"]
+    if level not in [choice_all, check_no_level]:
+        msg1 = f"check number '{number}' is not applicable for report type '{report}' and level '{level}'. "
+        msg2 = f"To view applicable check numbers, run the following: "
+        msg3 = f"'python genie-bpc-quac.py -c {cohort} -r upload -l {level} -o'"
+        raise ValueError(msg1 + msg2 + msg3)
+
+# for release report, ensure previous release is available for comparison
+if report == "release" and config["release"][cohort]["previous"] is None:
+    msg1 = f"cohort {cohort} does not have a previous release. "
+    msg2 = f"To view other available cohorts, run the following: "
+    msg3 = f"'python genie-bpc-quac.py -h'"
+    raise ValueError(msg1 + msg2 + msg3)
+
+# for comparison report, ensure previous table version is available
+if report == "comparison" and config["comparison"][cohort]["previous"] is None:
+    msg1 = f"cohort {cohort} does not have a previous table version for comparison. "
+    msg2 = f"To view other available cohorts, run the following: "
+    msg3 = f"'python genie-bpc-quac.py -h'"
+    raise ValueError(msg1 + msg2 + msg3)
+
+# for any report, site must be relevant for cohort
+if sites != choice_all and sites not in config["uploads"][cohort]:
+    msg1 = f"Site '{sites}' does not contribute data for the '{cohort}' cohort. "
+    msg2 = f"Valid sites for the '{cohort}' cohort: "
+    msg3 = f"{', '.join(config['uploads'][cohort])}"
+    raise ValueError(msg1 + msg2 + msg3)
+
+# parameter messaging ----------------------------------------
+
+if verbose:
+    print("Parameters: ")
+    print(f"- cohort:\t\t{cohort}")
+    print(f"- site(s):\t\t{sites}")
+    print(f"- report:\t\t{report}")
+    if report == "comparison":
+        print(f"  - previous:\t\t{config['comparison'][cohort]['previous']}")
+        print(f"  - current:\t\t{config['comparison'][cohort]['current']}")
+    if report == "release":
+        print(f"  - previous:\t\t{config['release'][cohort]['previous']}")
+        print(f"  - current:\t\t{config['release'][cohort]['current']}")
+    print(f"- level:\t\t{level}")
+    if number is not None:
+        print(f"- number:\t\t{number}")
+    print(f"- overview:\t\t{overview}")
+    print(f"- verbose:\t\t{verbose}")
+    print(f"- save on synapse:\t{save_synapse}")
+
+# level checks ----------------------------------------
+
+# storage
+valid_levels = [level]
+check_nos = []
+res = []
+outfile = ""
+n_issue = 0
+
+# format input
+if level == choice_all:
+    valid_levels = choices_level
+valid_levels = ["fail"] + valid_levels
+if number is None:
+    check_nos = config["report"][report]
+else:
+    check_nos = [number]
+
+if report in ["upload", "masking"] and sites == choice_all:
+    sites = list(
+        set(config["uploads"][cohort].keys()) - set(config["constants"]["sage"])
+    )
+else:
+    site = sites
+
+# collect relevant check functions
+check_nos_valid = [
+    check_no
+    for check_no in check_nos
+    if config["checks"][check_no]["deprecated"] == 0
+    and config["checks"][check_no]["implemented"] == 1
+    and config["checks"][check_no]["level"] in valid_levels
+]
+check_labels = [config["checks"][check_no]["label"] for check_no in check_nos_valid]
+check_level = [config["checks"][check_no]["level"] for check_no in check_nos_valid]
+check_fxns = get_check_functions(check_labels)
+
+if overview:
+    print(f"Checks ({len(check_fxns)}):")
+    for check_no in check_nos_valid:
+        print(
+            f"- {config['checks'][check_no]['level']} {str(check_no).zfill(2)} ({config['checks'][check_no]['label']}): {config['checks'][check_no]['description']} {config['checks'][check_no]['action']}"
+        )
+else:
+    if len(check_fxns):
+        for site in sites:
+            # run each applicable QA check
+            res = []
+            for i in range(len(check_fxns)):
+                fxn = check_fxns[i]
+                if verbose:
+                    print(
+                        f"{datetime.datetime.now()}: Checking '{list(check_fxns.keys())[i]}' for cohort '{cohort}' and site '{site}'..."
+                    )
+
+                res_check = check_fxns[i](
+                    cohort=cohort,
+                    site=None if site == choice_all else site,
+                    report=report,
+                )
+                if verbose:
+                    print(
+                        f" --> {0 if res_check is None or res_check.isna().all() else len(res_check)} {check_level[i]}(s) identified"
+                    )
+
+                res.append(res_check)
+
+                # check for flagged fail check
+                if (
+                    check_level[i] == "fail"
+                    and res_check is not None
+                    and len(res_check) > 0
+                ):
+                    raise Exception("Fail check flagged")
+
+            # write all issues to file
+            outfile = f"{cohort}_{site}_{report}_{level}.csv".lower()
+            if len(res):
+                issue_no = list(range(1, len(res) + 1))
+                pd.concat(
+                    [pd.DataFrame(issue_no, columns=["issue_no"]), pd.DataFrame(res)],
+                    axis=1,
+                ).to_csv(outfile, index=False)
+            else:
+                with open(outfile, "w") as f:
+                    f.write(f"No {level}s triggered.  Congrats! All done!")
+
+            # print number of detected issues
+            n_issue = 0 if len(res) == 0 else len(res)
+            if verbose and os.path.exists(outfile):
+                if level == choice_all:
+                    print(
+                        f"{datetime.datetime.now()}: Issues ({n_issue}) written to {outfile}"
+                    )
+                else:
+                    print(
+                        f"{datetime.datetime.now()}: {level.capitalize()}s ({n_issue}) written to {outfile}"
+                    )
+
+            if save_synapse and os.path.exists(outfile):
+                synid_folder_output = config["output"][cohort]
+
+                synid_file_output = save_to_synapse(
+                    path=outfile,
+                    parent_id=synid_folder_output,
+                    prov_name="GENIE BPC QA log",
+                    prov_desc=f"GENIE BPC QA {report} {level} log for cohort '{cohort}' and site '{site}'",
+                    prov_used=None,
+                    prov_exec="https://github.com/hhunterzinck/genie-bpc-quac/blob/develop/genie-bpc-quac.R",
+                )
+                syn.setAnnotations(
+                    synid_file_output,
+                    annotations={
+                        "cohort": cohort,
+                        "site": site,
+                        "level": level,
+                        "report": report,
+                        "issueCount": int(n_issue),
+                    },
+                )
+                os.remove(outfile)
+
+                if verbose:
+                    print(
+                        f"{datetime.datetime.now()}: Saved log to Synapse at '{outfile}' ({synid_file_output})"
+                    )
+    else:
+        if verbose:
+            print(
+                f"{datetime.datetime.now()}: No applicable {level}-level checks to perform."
+            )
+
+# wrap-up ----------------------------------------------------------------------
+
+# finish
+toc = time.time()
+if verbose:
+    print(f"{datetime.datetime.now()}: Runtime: {round(toc - tic)} s")
---
+++ utils.py
@@ -0,0 +1,439 @@
+import pandas as pd
+import synapseclient
+import re
+import pandas as pd
+import numpy as np
+import pytz
+from datetime import datetime
+
+syn = synapseclient.Synapse()
+syn.login()
+
+
+def is_synapse_table(synid):
+    if synid is None:
+        return False
+
+    entity = syn.get(synid, downloadFile=False)
+    file_type = entity.concreteType
+
+    if file_type is None:
+        return False
+
+    if "table" in file_type:
+        return True
+
+    return False
+
+
+def get_synid_file_name(synids):
+    file_names = []
+
+    for synid in synids:
+        ent = syn.get(synid)
+        file_names.append(ent.name)
+
+    return file_names
+
+
+def get_synid_from_table(synid, condition=None, with_names=False):
+    query = f"SELECT id, name FROM {synid}"
+    if condition is not None:
+        query += f" WHERE {condition}"
+
+    res = syn.tableQuery(query).asDataFrame()
+    ids = res["id"].tolist()
+
+    if with_names:
+        names = get_synid_file_name(ids)
+        return dict(zip(names, ids))
+
+    return ids
+
+
+def get_data(synid, version=None, sheet=1):
+    if is_synapse_table(synid):
+        if version is None:
+            query = f"SELECT * FROM {synid}"
+        else:
+            query = f"SELECT * FROM {synid}.{version}"
+
+        results = syn.tableQuery(query)
+        data = results.asDataFrame()
+    else:
+        ent = syn.get(synid, version=version)
+
+        if ent.name.endswith(".xlsx"):
+            data = pd.read_excel(ent.path, sheet_name=sheet)
+        else:
+            data = pd.read_csv(ent.path, na_values=[""])
+
+    return data
+
+
+def get_data_filtered_table(
+    synid, column_name, col_value, select=None, version=None, exact=None
+):
+    where_clause = ""
+    for i in range(len(column_name)):
+        if i == 0:
+            where_clause = "WHERE"
+        else:
+            where_clause += " AND"
+
+        if exact[i]:
+            where_clause += f" {column_name[i]} = '{col_value[i]}'"
+        else:
+            where_clause += f" {column_name[i]} LIKE '%{col_value[i]}%'"
+
+    select_clause = "*"
+    if select is not None:
+        select_clause = ", ".join(select)
+
+    if version is None:
+        query = f"SELECT {select_clause} FROM {synid} {where_clause}"
+    else:
+        if isinstance(version, list):
+            version = version[0]
+        query = f"SELECT {select_clause} FROM {synid}.{version} {where_clause}"
+
+    results = syn.tableQuery(query)
+    data = results.asDataFrame()
+
+    return data
+
+
+def get_data_filtered_file(
+    synid, column_name, col_value, select=None, version=None, exact=None
+):
+    ent = syn.get(synid, version=version)
+    data = pd.read_csv(ent.path, na_values=[""])
+
+    idx_filter = []
+    for i in range(len(column_name)):
+        if exact[i]:
+            idx_new = data[data[column_name[i]] == col_value[i]].index.tolist()
+        else:
+            idx_new = data[
+                data[column_name[i]].str.contains(col_value[i], regex=False)
+            ].index.tolist()
+
+        idx_filter += idx_new
+
+    idx_filter = list(set(idx_filter))
+
+    if len(idx_filter) > 0:
+        data = data.loc[idx_filter]
+    else:
+        data = None
+
+    if select is not None:
+        data = data[select]
+
+    return data
+
+
+def get_data_filtered(
+    synid, column_name, col_value, select=None, version=None, exact=None
+):
+    if is_synapse_table(synid):
+        data = get_data_filtered_table(
+            synid, column_name, col_value, select, version, exact
+        )
+    else:
+        data = get_data_filtered_file(
+            synid, column_name, col_value, select, version, exact
+        )
+
+    return data
+
+
+def get_entities_from_table(synapse_id, condition=None):
+    if condition is None:
+        query = f"SELECT id FROM {synapse_id}"
+    else:
+        query = f"SELECT id FROM {synapse_id} WHERE {condition}"
+
+    results = syn.tableQuery(query)
+    res = results.asDataFrame()
+
+    return res["id"].tolist()
+
+
+def get_synapse_folder_children(
+    synapse_id,
+    include_types=["folder", "file", "table", "link", "entityview", "dockerrepo"],
+):
+    children = syn.getChildren(synapse_id, includeTypes=include_types)
+    children_dict = {child["name"]: child["id"] for child in children}
+
+    return children_dict
+
+
+def get_folder_synid_from_path(synid_folder_root, path):
+    synid_folder_current = synid_folder_root
+    subfolders = path.split("/")
+
+    for subfolder in subfolders:
+        synid_folder_children = get_synapse_folder_children(
+            synid_folder_current, include_types=["folder"]
+        )
+
+        if subfolder not in synid_folder_children:
+            return None
+
+        synid_folder_current = synid_folder_children[subfolder]
+
+    return synid_folder_current
+
+
+def get_file_synid_from_path(synid_folder_root, path):
+    path_parts = path.split("/")
+    file_name = path_parts[-1]
+    path_abbrev = "/".join(path_parts[:-1])
+
+    synid_folder_dest = get_folder_synid_from_path(synid_folder_root, path_abbrev)
+    synid_folder_children = get_synapse_folder_children(
+        synid_folder_dest, include_types=["file"]
+    )
+
+    if file_name not in synid_folder_children:
+        return None
+
+    return synid_folder_children[file_name]
+
+
+# helper functions
+def get_added(current, previous):
+    return list(set(current) - set(previous))
+
+
+def get_removed(current, previous):
+    return list(set(previous) - set(current))
+
+
+def get_vector_index(array_index, nrow):
+    if len(array_index) != 2:
+        return None
+
+    row_index, col_index = array_index
+
+    if row_index > nrow:
+        return None
+
+    vector_index = (col_index - 1) * nrow + row_index
+    return vector_index
+
+
+def count_not_empty(vector, exclude=np.nan, na_strings=np.nan):
+    mod = vector.copy()
+
+    if not pd.isna(exclude):
+        mod = mod[~mod.isin([exclude])]
+
+    idx_na = mod.isna() | mod.isin([na_strings])
+    return len(mod) - sum(idx_na)
+
+
+def is_empty(vector, exclude=np.nan, na_strings=np.nan):
+    return count_not_empty(vector, exclude, na_strings) == 0
+
+
+def is_not_empty(vector, exclude=np.nan, na_strings=np.nan):
+    return bool(count_not_empty(vector, exclude, na_strings))
+
+
+def fraction_empty(vector, na_strings=np.nan):
+    return sum(vector.isin([na_strings])) / len(vector)
+
+
+def is_double(x):
+    try:
+        float(x)
+        return True
+    except ValueError:
+        return False
+
+
+def infer_data_type(x):
+    if is_double(x):
+        return "numeric"
+    return "character"
+
+
+def get_columns_added(data_current, data_previous):
+    return list(set(data_current.columns) - set(data_previous.columns))
+
+
+def get_columns_removed(data_current, data_previous):
+    return list(set(data_previous.columns) - set(data_current.columns))
+
+
+def get_column_elements_added(data_current, data_previous, column_name):
+    return list(set(data_current[column_name]) - set(data_previous[column_name]))
+
+
+def get_column_elements_removed(data_current, data_previous, column_name):
+    return list(set(data_previous[column_name]) - set(data_current[column_name]))
+
+
+def get_date_as_string(
+    ts, ts_format="%Y-%m-%dT%H:%M:%S", tz_current=None, tz_target="US/Pacific"
+):
+    if tz_current is None:
+        tz_current = datetime.now().astimezone().tzinfo
+
+    ts_posix = datetime.strptime(ts, ts_format).replace(
+        tzinfo=pytz.timezone(tz_current)
+    )
+    ts_mod = ts_posix.astimezone(pytz.timezone(tz_target)).strftime("%Y-%m-%d")
+
+    return ts_mod
+
+
+def is_timestamp_format_correct(timestamps, formats=["%Y-%m-%d %H:%M:%S"]):
+    for ts in timestamps:
+        for fmt in formats:
+            try:
+                datetime.strptime(ts, fmt)
+                return True
+            except ValueError:
+                pass
+    return False
+
+
+def is_date_format_correct(dates, formats=["%Y-%m-%d"]):
+    return is_timestamp_format_correct(dates, formats)
+
+
+def get_auth_token(path):
+    with open(path, "r") as file:
+        lines = file.readlines()
+
+    for line in lines:
+        if line.startswith("authtoken = "):
+            token = line.split(" ")[2].strip()
+            return token
+
+    return None
+
+
+def synLogin(auth=None, silent=True):
+    secret = os.getenv("SCHEDULED_JOB_SECRETS")
+
+    if secret:
+        syn = synapseclient.login(
+            silent=silent, authToken=json.loads(secret)["SYNAPSE_AUTH_TOKEN"]
+        )
+    elif auth is None or auth == "~/.synapseConfig":
+        syn = synapseclient.login(silent=silent)
+    else:
+        token = auth
+
+        if auth.endswith(".synapseConfig"):
+            token = get_auth_token(auth)
+
+            if token is None:
+                return False
+
+        try:
+            syn = synapseclient.login(authToken=token, silent=silent)
+        except Exception:
+            return False
+
+    return True
+
+
+def save_to_synapse(
+    path,
+    parent_id,
+    file_name=None,
+    prov_name=None,
+    prov_desc=None,
+    prov_used=None,
+    prov_exec=None,
+):
+    if file_name is None:
+        file_name = path
+
+    file = synapseclient.File(path=path, parentId=parent_id, name=file_name)
+
+    if (
+        prov_name is not None
+        or prov_desc is not None
+        or prov_used is not None
+        or prov_exec is not None
+    ):
+        act = synapseclient.Activity(
+            name=prov_name, description=prov_desc, used=prov_used, executed=prov_exec
+        )
+        file = syn.store(file, activity=act)
+    else:
+        file = syn.store(file)
+
+    return file.id
+
+
+def is_synapse_entity_csv(synapse_id):
+    try:
+        data = syn.get(synapse_id, downloadFile=False)
+        return data.concreteType == "org.sagebionetworks.repo.model.FileEntity"
+    except Exception:
+        return False
+
+
+def now(time_only=False, tz="US/Pacific"):
+    current_time = datetime.datetime.now(pytz.timezone(tz))
+
+    if time_only:
+        return current_time.strftime("%H:%M:%S")
+
+    return current_time.strftime("%Y-%m-%d %H:%M:%S")
+
+
+def wait_if_not(cond, msg=""):
+    if not cond:
+        print(msg)
+        print("Press control-C to exit and try again.")
+
+        while True:
+            pass
+
+
+def capitalize(str):
+    return str[0].upper() + str[1:].lower()
+
+
+def trim_string(str):
+    return str.strip()
+
+
+def merge_last_elements(x, delim):
+    return [x[0], delim.join(x[1:])]
+
+
+def strsplit_first(x, split):
+    unmerged = x.split(split)
+    remerge = merge_last_elements(unmerged, split)
+
+    return remerge
+
+
+def parse_mapping(str):
+    clean = trim_string(str.replace('"', ""))
+    splt = strsplit_first(clean.split("|")[0], ",")
+
+    codes = [trim_string(x[0]) for x in splt]
+    values = [trim_string(x[1]) for x in splt]
+    mapping = pd.DataFrame({"codes": codes, "values": values})
+
+    return mapping
+
+
+def parse_phase_from_cohort(cohort):
+    if cohort.endswith("2"):
+        return [cohort[:-1], 2]
+    elif cohort in ["RENAL", "MELANOMA", "OVARIAN", "ESOPHAGO"]:
+        return [cohort, 2]
+
+    return [cohort, 1]
